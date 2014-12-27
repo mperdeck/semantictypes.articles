773ff98f22e3079b75df8717dd35d2192fe24584
mperdeck <mperdeck@gmail.com>
2014-12-27 17:35:17 +1100

    1

---
+++ Reduce bugs in your code with semantic types/ArticleCode/ArticleCode/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArticleCode
+{
+    public class Person
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        public int myfunction(Person person)
+        {
+            return 0;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+
+
+        }
+    }
+}
